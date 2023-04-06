b66cea676fce1bac15f3a44081fec78c23bba272
Keane Osteen <osteenkeane@gmail.com>
2023-04-06 17:41:56 -0500

    added some new folders

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,20 +2,11 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div className='home-page'>
-      <h1 style={{
-        fontFamily: 'sans-serif',
-        fontSizeAdjust: 'initial',
-        fontSize: '150px',
-        color: 'white',
-        textAlign: 'center'
-      }}>Task Mate</h1>
       <div class='wrapper'>
         <ul class="dynamic-txts">
           <li><span>Don't just list, make progress</span></li>
           <li><span>From To-Do To Completion</span></li>
         </ul>
-      </div>
     </div>
   )
 }
