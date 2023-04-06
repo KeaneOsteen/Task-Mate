9fa9d33b2e8cca70b4e3700c5579a93b2a2b6c43
RobertNPC <rlmccrary1210@gmail.com>
2023-04-06 19:57:33 -0400

    made an about page and a mission statement

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import './About.css'
 
-export default function about() {
+export default function About() {
   return (
-    <div>
-        
+    <div className='page-content'>
+      <div className='mission-statement'>
+        <h2>About Us</h2>
+        <p>
+          Our mission is to simplify your life by providing an intuitive and powerful tool for managing your tasks and notes. 
+          We believe that staying organized shouldn't be a chore, and our app is designed to make it effortless. 
+          Our goal is to help you achieve more, both professionally and personally, by streamlining your workflow and reducing stress. 
+          Whether you're a student, a professional, or just someone who wants to stay on top of their to-do list, our app is the perfect 
+          solution for you. Join us on this journey towards greater productivity and peace of mind.
+        </p>
+      </div> 
     </div>
   )
 }
